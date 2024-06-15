2f25b4a1cd3270ab257b780d3829d70152791a79
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-06-15 16:47:55 +0530

    tests notion page to html

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,6 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-notion-client",
     platforms: [
+        .iOS(.v16),
         .macOS(.v13)
     ],
     products: [
