3e33e71585ae25ac954e5db0269e2582c2a334aa
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-07-07 20:42:17 +0530

    tests both normal page & page in a database

--- Sources/Exe/main.swift
+++ Sources/Exe/main.swift
@@ -1,11 +1,12 @@
 import Foundation
 import NotionClient
 
-//let id = "e97e43ab-fe17-4f47-a6ac-ccc258e4b2ea"
+//let id = "e97e43ab-fe17-4f47-a6ac-ccc258e4b2ea" // Page in Database
+//let id = "1f11dfa5-b02c-468a-b0a3-9024cc56d82e" // Normal Page
 //do {
 //    let response = try await fetchPageContent(by: id)
 //
-//    let data = try JSONEncoder().encode(response)
+//    let data = try JSONEncoder.notion.encode(response)
 //    let json = String(data: data, encoding: .utf8)!
 //    print(json)
 //} catch {
