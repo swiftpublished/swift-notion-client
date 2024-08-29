3f5aff247a20091020114b547c0c024af84cc753
Shahrukh Alam <alam.shahrukh786@gmail.com>
2024-08-29 22:11:13 +0530

    [Test 15] FoundationNetworking for linux may not be needed

--- Sources/Client/Client.swift
+++ Sources/Client/Client.swift
@@ -1,7 +1,7 @@
 import Foundation
-#if canImport(FoundationNetworking)
-import FoundationNetworking
-#endif
+//#if canImport(FoundationNetworking)
+//import FoundationNetworking
+//#endif
 
 import NotionParsing
 
